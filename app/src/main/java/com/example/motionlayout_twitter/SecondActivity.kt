a7791539a47fa4fbe17dfaac2fdbcf97f6903dbd
Waseef Akhtar <waseef@degoo.com>
2020-06-16 00:38:10 +0500

    Moving to new activity from splash

--- app/src/main/java/com/example/motionlayout_twitter/SecondActivity.kt
+++ app/src/main/java/com/example/motionlayout_twitter/SecondActivity.kt
@@ -1,35 +1,14 @@
 package com.example.motionlayout_twitter
 
 import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.constraintlayout.motion.widget.MotionLayout
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@MainActivity, SecondActivity::class.java))
-            }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
-
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
-
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
-        })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        motionLayout.startLayoutAnimation()
-
+        setContentView(R.layout.activity_second)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
